02741cbb1e4931f0043fa2a9117a9cc3412310f6
Aniruddha <146610432+Anirudh-x@users.noreply.github.com>
2024-06-08 00:19:27 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+const form = {
+  signInForm() {
+    document.querySelector('.form').innerHTML = `
+      <form>
+        <button class="close-button">x</button>
+        <h3>Welcome Back !</h3>
+      
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username" class="sign-page-input">
+      
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password" class="sign-page-input">
+      
+        <button class="sign-page-button">Sign In</button>
+        <div class="social">
+          <div class="go"><i class="fab fa-google"></i>  Google</div>
+          <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+        </div>
+      </form>
+    `
+  },
+  
+  signUpForm() {
+    document.querySelector('.form').innerHTML = `
+      <form>
+        <button class="close-button">x</button>
+        <h3>Welcome To Travelio !</h3>
+      
+        <label for="username">Username</label>
+        <input type="text" placeholder="Username" id="username" class="sign-page-input">
+  
+        <label for="username">Email</label>
+        <input type="text" placeholder="Email or Phone" id="username" class="sign-page-input">
+      
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password" class="sign-page-input">
+      
+        <button class="sign-page-button">Create an Account</button>
+        <div class="social">
+          <div class="go"><i class="fab fa-google"></i>  Google</div>
+          <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+        </div>
+      </form>
+    `;
+  },
+}
+
+
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
 let thumbnail = document.getElementsByClassName('thumbnail');
@@ -11,8 +59,6 @@ let backgroundImg = new Array(
   "images/bg6.png",
   "images/bg7.png",
 );
-
-let exploreButton = document.querySelector('.js-explore')
 
 let i = 0;
 next.onclick = function() {
@@ -31,3 +77,23 @@ prev.onclick = function() {
     i--;
   }
 }
+
+
+document.querySelector('.sign-in').addEventListener('click', () => {
+
+  document.querySelector('.sign-div').classList.add('form');
+
+  form.signInForm();
+})
+
+document.querySelector('.sign-up').addEventListener('click', () => {
+
+  document.querySelector('.sign-div').classList.add('form');
+
+  form.signUpForm();
+})
+
+document.querySelector('.close-button').addEventListener('click', () => {
+  document.querySelector('.sign-div').classList.remove('form')
+})
+
